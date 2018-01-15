8d3818dee54dc5f8fec245396944e2cd05de7a2f
Muzubi <sylvaincallewaert@rocketmail.com>
2018-01-15 20:50:04 +0100

    Récupération d'une saisie user vers un textview et un toast

--- app/src/main/java/com/example/muzubi/maelysproject/Apprendre.java
+++ app/src/main/java/com/example/muzubi/maelysproject/Apprendre.java
@@ -3,8 +3,15 @@ package com.example.muzubi.maelysproject;
 import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Apprendre extends AppCompatActivity {
+
+    private EditText editTextApprendre;
+    private TextView textViewApprendre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +23,20 @@ public class Apprendre extends AppCompatActivity {
         if(actionBar != null){
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        //Instanciation
+        this.editTextApprendre = (EditText) findViewById(R.id.editTextApprendre) ;
+        this.textViewApprendre =  (TextView) findViewById(R.id.textViewApprendre);
+
+    }
+
+    public void onClickValider(View view) {
+        //Récupérer la saisie d'un edittext vers un testview
+        String text = editTextApprendre.getText().toString();
+        textViewApprendre.setText(text);
+        //Afficher dans un toast le text validé
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+
 
     }
 }
